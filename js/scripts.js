7e63c1f576568c57b6895afc920191c07c95848f
Anitha987 <anithaumuhire@gmail.com>
2019-07-28 13:03:08 +0200

    add paragraph element

---
+++ js/scripts.js
@@ -0,0 +1,26 @@
+$(document).ready(function(){
+    $("#design").click(function(){
+        $("#design").hide();
+        $("#anny").show();
+    });
+    $("#anny").click(function(){
+        $("#anny").hide();
+        $("#design").show();
+    });
+     $("#develop").click(function(){
+        $("#develop").hide();
+    $("#anit").show();
+    });
+     $("#anit").click(function(){
+        $("#anit").hide();
+        $("#develop").show();
+    });
+    $("#product").click(function(){
+        $("#anitha").hide();
+       $("#product").show();
+    });
+    $("#anitha").click(function(){
+       $("#anitha").hide();
+      $("#product").show();
+     });
+});
